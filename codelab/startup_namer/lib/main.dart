f0a6bb82819599dd27fed31b774f67695a685ffb
droidYu <droidyu@foxmail.com>
2022-04-03 09:23:13 +0800

    add package

--- codelab/startup_namer/lib/main.dart
+++ codelab/startup_namer/lib/main.dart
@@ -1,16 +1,23 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
-void main()=>runApp(const MyApp());
+void main() => runApp(const MyApp());
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: "Flutter",
       home: Scaffold(
-        appBar: AppBar(title:const Text("Flutter"),),
-        body:const Center(child: Text("Hello Flutter!"),),
+        appBar: AppBar(
+          title: const Text("Flutter"),
+        ),
+        body:  Center(
+          child: Text(wordPair.asPascalCase),
+        ),
       ),
     );
   }
