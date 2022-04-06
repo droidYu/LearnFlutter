00558df0ed198c495ae8ee5e1c555bda6cfec5ee
droidYu <droidyu@foxmail.com>
2022-04-06 16:55:29 +0800

    manage state by parent

--- book/flutter_in_action_2nd/first_flutter_app/lib/_2/state_manage.dart
+++ book/flutter_in_action_2nd/first_flutter_app/lib/_2/state_manage.dart
@@ -11,9 +11,7 @@ class StateManageRoute extends StatelessWidget {
       ),
       body: Center(
         child: Column(
-          children: const [
-            TapboxA(),
-          ],
+          children: const [TapboxA(), ParentWidgetB()],
         ),
       ),
     );
@@ -60,4 +58,64 @@ class _TapboxAState extends State<TapboxA> {
   }
 }
 
+// ParentWidgetB 为 TapboxB 管理状态.
 
+//------------------------ ParentWidgetB --------------------------------
+
+class ParentWidgetB extends StatefulWidget {
+  const ParentWidgetB({Key? key}) : super(key: key);
+
+  @override
+  State<ParentWidgetB> createState() => _ParentWidgetBState();
+}
+
+class _ParentWidgetBState extends State<ParentWidgetB> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = !_active;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TapboxB(
+      active: _active,
+      onChanged: _handleTapboxChanged,
+    );
+  }
+}
+
+//------------------------- TapboxB ----------------------------------
+
+class TapboxB extends StatelessWidget {
+  const TapboxB({Key? key, this.active = false, required this.onChanged})
+      : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            active ? 'Active' : 'Inactive',
+            style: const TextStyle(fontSize: 32, color: Colors.white),
+          ),
+        ),
+        width: 150,
+        height: 150,
+        decoration: BoxDecoration(
+            color: active ? Colors.lightGreen[700] : Colors.grey[600]),
+      ),
+    );
+  }
+}
