a39339cfa0b7e96ba930d2733c23778a61f9d3b5
droidYu <droidyu@foxmail.com>
2022-04-07 21:37:55 +0800

    form

--- book/flutter_in_action_2nd/first_flutter_app/lib/_3/input_and_form.dart
+++ book/flutter_in_action_2nd/first_flutter_app/lib/_3/input_and_form.dart
@@ -13,6 +13,10 @@ class _InputAndFormPageState extends State<InputAndFormPage> {
   FocusNode focusNode2 = FocusNode();
   FocusScopeNode? focusScopeNode;
 
+  final _formUsernameController = TextEditingController();
+  final _formPasswordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
   @override
   void initState() {
     super.initState();
@@ -38,99 +42,174 @@ class _InputAndFormPageState extends State<InputAndFormPage> {
         ),
         body: SingleChildScrollView(
           child: Column(
-            children: [
-              TextField(
-                controller: _usernameController,
-                onChanged: (value) {
-                  print('onChanged:$value');
-                },
-                decoration: const InputDecoration(
-                    labelText: 'username',
-                    hintText: 'input username',
-                    prefixIcon: Icon(Icons.person)),
-              ),
-              const TextField(
-                decoration: InputDecoration(
-                    labelText: 'password',
-                    hintText: 'input password',
-                    prefixIcon: Icon(Icons.lock)),
-                obscureText: true,
-              ),
-              TextField(
-                autofocus: true,
-                focusNode: focusNode1,
-                decoration: const InputDecoration(labelText: 'input1'),
-              ),
-              TextField(
-                focusNode: focusNode2,
-                decoration: const InputDecoration(labelText: 'input2'),
-              ),
-              Builder(
-                  builder: (context) => Column(
-                        children: [
-                          ElevatedButton(
-                              onPressed: () {
-                                focusScopeNode ??= FocusScope.of(context);
-                                focusScopeNode?.requestFocus(focusNode2);
-                              },
-                              child: const Text('move focus to input2')),
-                          ElevatedButton(
-                              onPressed: () {
-                                focusNode1.unfocus();
-                                focusNode2.unfocus();
-                              },
-                              child: const Text('hide keyboard'))
-                        ],
-                      )),
-              const TextField(
-                decoration: InputDecoration(
-                    labelText: 'custom',
-                    prefixIcon: Icon(Icons.person),
-                    enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.green)),
-                    focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.blue))),
-              ),
-              Theme(
-                  data: Theme.of(context).copyWith(
-                      hintColor: Colors.green,
-                      inputDecorationTheme: const InputDecorationTheme(
-                          labelStyle: TextStyle(color: Colors.black),
-                          hintStyle:
-                              TextStyle(color: Colors.grey, fontSize: 14))),
-                  child: Column(
-                    children: const [
-                      TextField(
-                        decoration: InputDecoration(
-                            labelText: 'username',
-                            hintText: 'input username',
-                            prefixIcon: Icon(Icons.person)),
-                      ),
-                      TextField(
-                        decoration: InputDecoration(
-                            labelText: 'password',
-                            hintText: 'input password',
-                            prefixIcon: Icon(Icons.lock),
-                            hintStyle:
-                                TextStyle(color: Colors.green, fontSize: 10)),
-                      ),
-                    ],
-                  )),
-              Container(
-                child: const TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                      labelText: 'email',
-                      hintText: 'input email',
-                      prefixIcon: Icon(Icons.email),
-                      border: InputBorder.none),
-                ),
-                decoration: const BoxDecoration(
-                    border: Border(
-                        bottom: BorderSide(color: Colors.grey, width: 1))),
-              )
-            ],
+            children: [getInput(), getForm()],
           ),
+        ));
+  }
+
+  Widget getInput() {
+    return Column(
+      children: [
+        TextField(
+          controller: _usernameController,
+          onChanged: (value) {
+            print('onChanged:$value');
+          },
+          decoration: const InputDecoration(
+              labelText: 'username',
+              hintText: 'input username',
+              prefixIcon: Icon(Icons.person)),
+        ),
+        const TextField(
+          decoration: InputDecoration(
+              labelText: 'password',
+              hintText: 'input password',
+              prefixIcon: Icon(Icons.lock)),
+          obscureText: true,
+        ),
+        TextField(
+          autofocus: true,
+          focusNode: focusNode1,
+          decoration: const InputDecoration(labelText: 'input1'),
+        ),
+        TextField(
+          focusNode: focusNode2,
+          decoration: const InputDecoration(labelText: 'input2'),
+        ),
+        Builder(
+            builder: (context) => Column(
+                  children: [
+                    ElevatedButton(
+                        onPressed: () {
+                          focusScopeNode ??= FocusScope.of(context);
+                          focusScopeNode?.requestFocus(focusNode2);
+                        },
+                        child: const Text('move focus to input2')),
+                    ElevatedButton(
+                        onPressed: () {
+                          focusNode1.unfocus();
+                          focusNode2.unfocus();
+                        },
+                        child: const Text('hide keyboard'))
+                  ],
+                )),
+        const TextField(
+          decoration: InputDecoration(
+              labelText: 'custom',
+              prefixIcon: Icon(Icons.person),
+              enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.green)),
+              focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue))),
+        ),
+        Theme(
+            data: Theme.of(context).copyWith(
+                hintColor: Colors.green,
+                inputDecorationTheme: const InputDecorationTheme(
+                    labelStyle: TextStyle(color: Colors.black),
+                    hintStyle: TextStyle(color: Colors.grey, fontSize: 14))),
+            child: Column(
+              children: const [
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: 'username',
+                      hintText: 'input username',
+                      prefixIcon: Icon(Icons.person)),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      labelText: 'password',
+                      hintText: 'input password',
+                      prefixIcon: Icon(Icons.lock),
+                      hintStyle: TextStyle(color: Colors.green, fontSize: 10)),
+                ),
+              ],
+            )),
+        Container(
+          child: const TextField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+                labelText: 'email',
+                hintText: 'input email',
+                prefixIcon: Icon(Icons.email),
+                border: InputBorder.none),
+          ),
+          decoration: const BoxDecoration(
+              border: Border(bottom: BorderSide(color: Colors.grey, width: 1))),
+        )
+      ],
+    );
+  }
+
+  Widget getForm() {
+    return Form(
+        key: _formKey,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        child: Column(
+          children: [
+            TextFormField(
+              controller: _formUsernameController,
+              decoration: const InputDecoration(
+                  labelText: 'username',
+                  hintText: 'input username',
+                  prefixIcon: Icon(Icons.person)),
+              validator: (value) {
+                return value!.trim().isNotEmpty ? null : "username can't none";
+              },
+            ),
+            TextFormField(
+              controller: _formPasswordController,
+              decoration: const InputDecoration(
+                  labelText: 'password',
+                  hintText: 'input password',
+                  prefixIcon: Icon(Icons.lock)),
+              validator: (value) {
+                return value!.trim().length > 5
+                    ? null
+                    : "password length can't less than 6";
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 28),
+                  child: ElevatedButton(
+                    child: const Padding(
+                      padding: EdgeInsets.all(16),
+                      child: Text('Login'),
+                    ),
+                    onPressed: () {
+                      if ((_formKey.currentState as FormState).validate()) {
+                        print('Login Success');
+                      } else {
+                        print('Login Error');
+                      }
+                    },
+                  ),
+                ),
+                Padding(
+                    padding: const EdgeInsets.only(top: 28, left: 32),
+                    child: Builder(
+                      builder: (context) {
+                        return ElevatedButton(
+                          child: const Padding(
+                            padding: EdgeInsets.all(16),
+                            child: Text('Login'),
+                          ),
+                          onPressed: () {
+                            if (Form.of(context)!.validate()) {
+                              print('Login Success');
+                            } else {
+                              print('Login Error');
+                            }
+                          },
+                        );
+                      },
+                    ))
+              ],
+            )
+          ],
         ));
   }
 }
