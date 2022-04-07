954a86a768f46a0f090de032a739d25004927c93
droidYu <droidyu@foxmail.com>
2022-04-07 14:05:18 +0800

    TextField focus

--- book/flutter_in_action_2nd/first_flutter_app/lib/_3/input_and_form.dart
+++ book/flutter_in_action_2nd/first_flutter_app/lib/_3/input_and_form.dart
@@ -9,6 +9,9 @@ class InputAndFormPage extends StatefulWidget {
 
 class _InputAndFormPageState extends State<InputAndFormPage> {
   final TextEditingController _usernameController = TextEditingController();
+  FocusNode focusNode1 = FocusNode();
+  FocusNode focusNode2 = FocusNode();
+  FocusScopeNode? focusScopeNode;
 
   @override
   void initState() {
@@ -19,36 +22,68 @@ class _InputAndFormPageState extends State<InputAndFormPage> {
     _usernameController.addListener(() {
       print('addListener:${_usernameController.text}');
     });
+    focusNode1.addListener(() {
+      print('input1 has focus?${focusNode1.hasFocus}');
+    });
+    focusNode2.addListener(() {
+      print('input2 has focus?${focusNode2.hasFocus}');
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('InputAndForm'),
-      ),
-      body: Column(
-        children: [
-          TextField(
-            autofocus: true,
-            controller: _usernameController,
-            onChanged: (value) {
-              print('onChanged:$value');
-            },
-            decoration: const InputDecoration(
-                labelText: 'username',
-                hintText: 'input username',
-                prefixIcon: Icon(Icons.person)),
+        appBar: AppBar(
+          title: const Text('InputAndForm'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              TextField(
+                controller: _usernameController,
+                onChanged: (value) {
+                  print('onChanged:$value');
+                },
+                decoration: const InputDecoration(
+                    labelText: 'username',
+                    hintText: 'input username',
+                    prefixIcon: Icon(Icons.person)),
+              ),
+              const TextField(
+                decoration: InputDecoration(
+                    labelText: 'password',
+                    hintText: 'input password',
+                    prefixIcon: Icon(Icons.lock)),
+                obscureText: true,
+              ),
+              TextField(
+                autofocus: true,
+                focusNode: focusNode1,
+                decoration: const InputDecoration(labelText: 'input1'),
+              ),
+              TextField(
+                focusNode: focusNode2,
+                decoration: const InputDecoration(labelText: 'input2'),
+              ),
+              Builder(
+                  builder: (context) => Column(
+                        children: [
+                          ElevatedButton(
+                              onPressed: () {
+                                focusScopeNode ??= FocusScope.of(context);
+                                focusScopeNode?.requestFocus(focusNode2);
+                              },
+                              child: const Text('move focus to input2')),
+                          ElevatedButton(
+                              onPressed: () {
+                                focusNode1.unfocus();
+                                focusNode2.unfocus();
+                              },
+                              child: const Text('hide keyboard'))
+                        ],
+                      ))
+            ],
           ),
-          const TextField(
-            decoration: InputDecoration(
-                labelText: 'password',
-                hintText: 'input password',
-                prefixIcon: Icon(Icons.lock)),
-            obscureText: true,
-          )
-        ],
-      ),
-    );
+        ));
   }
 }
