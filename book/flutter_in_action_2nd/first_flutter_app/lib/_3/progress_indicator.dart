315d466560c0f081ee4bf4e02f348aa3e6d8e8be
droidYu <droidyu@foxmail.com>
2022-04-08 11:00:22 +0800

    progress indicator color animation

--- book/flutter_in_action_2nd/first_flutter_app/lib/_3/progress_indicator.dart
+++ book/flutter_in_action_2nd/first_flutter_app/lib/_3/progress_indicator.dart
@@ -7,31 +7,51 @@ class ProgressIndicatorPage extends StatefulWidget {
   State<ProgressIndicatorPage> createState() => _ProgressIndicatorPageState();
 }
 
-class _ProgressIndicatorPageState extends State<ProgressIndicatorPage> {
+class _ProgressIndicatorPageState extends State<ProgressIndicatorPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 10));
+    _controller.forward();
+    _controller.addListener(() {
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ProgressIndicator'),
+        title: const Text('ProgressIndicator'),
       ),
       body: Column(
         children: [
           LinearProgressIndicator(
             backgroundColor: Colors.grey[200],
-            valueColor: AlwaysStoppedAnimation(Colors.blue),
+            valueColor: const AlwaysStoppedAnimation(Colors.blue),
           ),
           LinearProgressIndicator(
             backgroundColor: Colors.grey[200],
-            valueColor: AlwaysStoppedAnimation(Colors.blue),
+            valueColor: const AlwaysStoppedAnimation(Colors.blue),
             value: .5,
           ),
           CircularProgressIndicator(
             backgroundColor: Colors.grey[200],
-            valueColor: AlwaysStoppedAnimation(Colors.blue),
+            valueColor: const AlwaysStoppedAnimation(Colors.blue),
           ),
           CircularProgressIndicator(
             backgroundColor: Colors.grey[200],
-            valueColor: AlwaysStoppedAnimation(Colors.blue),
+            valueColor: const AlwaysStoppedAnimation(Colors.blue),
             value: .5,
             strokeWidth: 20,
           ),
@@ -39,7 +59,7 @@ class _ProgressIndicatorPageState extends State<ProgressIndicatorPage> {
             height: 30,
             child: LinearProgressIndicator(
               backgroundColor: Colors.grey[200],
-              valueColor: AlwaysStoppedAnimation(Colors.blue),
+              valueColor: const AlwaysStoppedAnimation(Colors.blue),
               value: .5,
             ),
           ),
@@ -49,12 +69,18 @@ class _ProgressIndicatorPageState extends State<ProgressIndicatorPage> {
             child: CircularProgressIndicator(
               value: .7,
               backgroundColor: Colors.grey[200],
-              valueColor: AlwaysStoppedAnimation(Colors.blue),
+              valueColor: const AlwaysStoppedAnimation(Colors.blue),
             ),
+          ),
+          LinearProgressIndicator(
+            backgroundColor: Colors.grey[200],
+            valueColor: ColorTween(begin: Colors.grey, end: Colors.blue)
+                .animate(_controller),
+            value: _controller.value,
           )
         ]
             .map((e) => Padding(
-                  padding: EdgeInsets.all(16),
+          padding: const EdgeInsets.all(16),
                   child: e,
                 ))
             .toList(),
