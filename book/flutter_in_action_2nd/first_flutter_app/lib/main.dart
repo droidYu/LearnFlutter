ca7c6d033313d596fd34102f332e0a6d7fdcf4bf
droidYu <droidyu@foxmail.com>
2022-04-08 12:08:32 +0800

    update

--- book/flutter_in_action_2nd/first_flutter_app/lib/main.dart
+++ book/flutter_in_action_2nd/first_flutter_app/lib/main.dart
@@ -1,32 +1,59 @@
 import 'package:first_flutter_app/_2/route_manage.dart';
+import 'package:first_flutter_app/_2/state_manage.dart';
+import 'package:first_flutter_app/_2/widget.dart';
+import 'package:first_flutter_app/_3/button.dart';
+import 'package:first_flutter_app/_3/image_and_icon.dart';
+import 'package:first_flutter_app/_3/input_and_form.dart';
 import 'package:first_flutter_app/_3/progress_indicator.dart';
+import 'package:first_flutter_app/_3/switch_and_checkbox.dart';
+import 'package:first_flutter_app/_3/text.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  final routes = {
+    'new_page': (context) => const NewRoute(),
+    'widget': (context) => const StatelessWidgetRoute(),
+    'context': (context) => const ContextRoute(),
+    'state': (context) => const GetStateObjectRoute(),
+    'cupertino': (context) => const CupertinoTestRoute(),
+    'state_manage': (context) => const StateManageRoute(),
+    'route': (context) => const FirstRoute(),
+    'text': (context) => const TextPage(),
+    'button': (context) => const ButtonPage(),
+    'image': (context) => const ImageAndIconPage(),
+    'switch': (context) => const SwitchAndCheckboxPage(),
+    'input': (context) => const InputAndFormPage(),
+    'progress': (context) => const ProgressIndicatorPage()
+  };
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Flutter",
       initialRoute: '/',
-      routes: {
-        'new_page': (context) => const NewRoute(),
-        // '/': (context) => const StatelessWidgetRoute()
-        // '/': (context) => const ContextRoute()
-        // '/': (context) => const GetStateObjectRoute()
-        // '/': (context) => const CupertinoTestRoute()
-        // '/': (context) => const StateManageRoute()
-        // '/': (context) => const FirstRoute()
-        // '/': (context) => const TextPage()
-        // '/': (context) => const ButtonPage()
-        // '/': (context) => const ImageAndIconPage()
-        // '/': (context) => const SwitchAndCheckboxPage()
-        // '/': (context) => const InputAndFormPage()
-        '/': (context) => const ProgressIndicatorPage()
-      },
+      routes: routes,
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Learn Flutter'),
+          ),
+          body: Builder(
+            builder: (context) => ListView(
+              children: ListTile.divideTiles(
+                  context: context,
+                  tiles: routes.keys.map((e) => ListTile(
+                        title: GestureDetector(
+                          child: Text(e),
+                          onTap: () {
+                            Navigator.pushNamed(context, e);
+                          },
+                        ),
+                      ))).toList(),
+            ),
+          )),
     );
   }
 }
