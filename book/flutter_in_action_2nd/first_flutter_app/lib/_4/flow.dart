43993d86c6d4aeed712f4b821c5d574d4881e2f1
droidYu <droidyu@foxmail.com>
2022-04-08 20:17:38 +0800

    Flow

--- book/flutter_in_action_2nd/first_flutter_app/lib/_4/flow.dart
+++ book/flutter_in_action_2nd/first_flutter_app/lib/_4/flow.dart
@@ -62,8 +62,80 @@ class FlowPage extends StatelessWidget {
                   ),
                 ),
               ],
+            ),
+            Flow(
+              delegate: TestFlowDelegate(margin: const EdgeInsets.all(10)),
+              children: [
+                Container(
+                  width: 80,
+                  height: 80,
+                  color: Colors.red,
+                ),
+                Container(
+                  width: 80,
+                  height: 80,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: 80,
+                  height: 80,
+                  color: Colors.blue,
+                ),
+                Container(
+                  width: 80,
+                  height: 80,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  width: 80,
+                  height: 80,
+                  color: Colors.brown,
+                ),
+                Container(
+                  width: 80,
+                  height: 80,
+                  color: Colors.purple,
+                ),
+              ],
             )
           ],
         ));
+  }
+}
+
+class TestFlowDelegate extends FlowDelegate {
+  late EdgeInsets margin;
+
+  TestFlowDelegate({this.margin = EdgeInsets.zero});
+
+  double width = 0;
+  double height = 0;
+
+  @override
+  void paintChildren(FlowPaintingContext context) {
+    var x = margin.left;
+    var y = margin.top;
+    for (int i = 0; i < context.childCount; i++) {
+      var w = context.getChildSize(i)!.width + x + margin.right;
+      if (w < context.size.width) {
+        context.paintChild(i, transform: Matrix4.translationValues(x, y, 0));
+        x = w + margin.left;
+      } else {
+        x = margin.left;
+        y += context.getChildSize(i)!.height + margin.top + margin.bottom;
+        context.paintChild(i, transform: Matrix4.translationValues(x, y, 0));
+        x += context.getChildSize(i)!.width + margin.left + margin.right;
+      }
+    }
+  }
+
+  @override
+  Size getSize(BoxConstraints constraints) {
+    return const Size(double.infinity, 200);
+  }
+
+  @override
+  bool shouldRepaint(FlowDelegate oldDelegate) {
+    return oldDelegate != this;
   }
 }
