f81d90a7e63fad35164bf581132381a067149e3a
droidYu <droidyu@foxmail.com>
2022-04-08 10:45:58 +0800

    progress indicator custom size

--- book/flutter_in_action_2nd/first_flutter_app/lib/_3/progress_indicator.dart
+++ book/flutter_in_action_2nd/first_flutter_app/lib/_3/progress_indicator.dart
@@ -34,6 +34,23 @@ class _ProgressIndicatorPageState extends State<ProgressIndicatorPage> {
             valueColor: AlwaysStoppedAnimation(Colors.blue),
             value: .5,
             strokeWidth: 20,
+          ),
+          SizedBox(
+            height: 30,
+            child: LinearProgressIndicator(
+              backgroundColor: Colors.grey[200],
+              valueColor: AlwaysStoppedAnimation(Colors.blue),
+              value: .5,
+            ),
+          ),
+          SizedBox(
+            height: 100,
+            width: 200,
+            child: CircularProgressIndicator(
+              value: .7,
+              backgroundColor: Colors.grey[200],
+              valueColor: AlwaysStoppedAnimation(Colors.blue),
+            ),
           )
         ]
             .map((e) => Padding(
