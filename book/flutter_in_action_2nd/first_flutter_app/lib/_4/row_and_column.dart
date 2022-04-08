140e9526c901b92a127eafd42a2ac09123df19e0
droidYu <droidyu@foxmail.com>
2022-04-08 19:26:22 +0800

    Column

--- book/flutter_in_action_2nd/first_flutter_app/lib/_4/row_and_column.dart
+++ book/flutter_in_action_2nd/first_flutter_app/lib/_4/row_and_column.dart
@@ -6,48 +6,31 @@ class RowAndColumnPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('RowAndColumn'),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text(" hello world "),
-              Text(" I am Jack "),
-            ],
+        appBar: AppBar(
+          title: const Text('RowAndColumn'),
+        ),
+        body: Container(
+          color: Colors.green,
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Expanded(
+                    child: Container(
+                  color: Colors.red,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children: const [
+                      Text("hello world "),
+                      Text("I am Jack "),
+                    ],
+                  ),
+                ))
+              ],
+            ),
           ),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text(" hello world "),
-              Text(" I am Jack "),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            textDirection: TextDirection.rtl,
-            children: const [
-              Text(" hello world "),
-              Text(" I am Jack "),
-            ],
-          ),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            verticalDirection: VerticalDirection.up,
-            children: const [
-              Text(
-                " hello world ",
-                style: TextStyle(fontSize: 30),
-              ),
-              Text(" I am Jack "),
-            ],
-          )
-        ],
-      ),
-    );
+        ));
   }
 }
